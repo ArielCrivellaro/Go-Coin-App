8752c2f266646412aaccf23d28ffe0435f5d5667
Ariel Crivellaro Martins <arielcrivellaro@gmail.com>
2020-09-19 10:29:39 -0300

    add Signin page

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+    font-size: 24px;
+    color: #f4ede8;
+    margin: 24px 0 24px;
+`;
